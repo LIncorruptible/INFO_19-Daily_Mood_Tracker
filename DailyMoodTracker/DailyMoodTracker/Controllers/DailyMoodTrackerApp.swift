35ea6dbca5d3460acde10395a46e10bdd169c214
Adrien222 <adrienchretien2002@gmail.com>
2024-12-29 11:25:19 +0100

    Front - Ajout de la view Dashboard

--- DailyMoodTracker/DailyMoodTracker/Controllers/DailyMoodTrackerApp.swift
+++ DailyMoodTracker/DailyMoodTracker/Controllers/DailyMoodTrackerApp.swift
@@ -1,10 +1,3 @@
-//
-//  DailyMoodTrackerApp.swift
-//  DailyMoodTracker
-//
-//  Created by etudiant on 10/12/2024.
-//
-
 import SwiftUI
 import SwiftData
 
@@ -24,7 +17,7 @@ struct DailyMoodTrackerApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            DashboardView()  // <-- On affiche DashboardView en premier
                 .onAppear {
                     importQuotesIfNeeded()
                     importActivitiesIfNeeded()
@@ -35,39 +28,29 @@ struct DailyMoodTrackerApp: App {
 
     private func importQuotesIfNeeded() {
         let context = sharedModelContainer.mainContext
-
-        // Créez un FetchDescriptor pour le type Quote
+        
         let fetchDescriptor = FetchDescriptor<Quote>()
-
         do {
-            // On récupère le tableau d’objets de type Quote
             let quotes = try context.fetch(fetchDescriptor)
-
             if quotes.isEmpty {
-                // S’il n’y en a aucun, on importe
                 QuoteImporter.importQuotes(from: "citations", context: context)
             }
-
         } catch {
-            print("Erreur lors de la récupération des citations : (error)")
+            print("Erreur lors de la récupération des citations : \(error)")
         }
     }
 
     private func importActivitiesIfNeeded() {
         let context = sharedModelContainer.mainContext
-
-        // Pareil pour Activity
+        
         let fetchDescriptor = FetchDescriptor<Activity>()
-
         do {
             let activities = try context.fetch(fetchDescriptor)
-
             if activities.isEmpty {
                 ActivityImporter.importActivities(from: "activities", context: context)
             }
-
         } catch {
-            print("Erreur lors de la récupération des activités : (error)")
+            print("Erreur lors de la récupération des activités : \(error)")
         }
     }
 }
